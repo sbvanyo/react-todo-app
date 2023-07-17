9a072238c00b21f124d05c7ac711c2a2710f5605
Stacey Vanyo <sbvanyo@gmail.com>
2023-07-17 13:38:31 -0500

    all functional

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,28 @@ export default function App() {
         { id: crypto.randomUUID(), title: newItem, completed: false },
       ]
     })
+  // Clears input box after each submit by setting it to an empty string
+    setNewItem("")
+  }
+  // Takes in a todo's id and whether or not it is completed. We want to update our todos to change the id of the todo passed into this func to be completed. Map through all todos and for each one, check to see if it's the one I'm currently trying to toggle
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          // Creates a brand new todo, spreads it out so we get a brand new state object, and changes one property on it (checked/completed) (since state is immutable and we can't change the original)
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      // Return a filtered version of currentTodos to show all todos except the one we want to remove. "If my todo id is not equal to this id, keep it, otherwise remove it"
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   console.log(todos);
@@ -34,22 +56,28 @@ export default function App() {
     </div>
     <button className="btn">Add</button>
   </form>
-  <h1 className="header">To-do List</h1>
+  <h1 className="header">Todo List</h1>
   <ul className="list">
-    <li>
+    {/* When there are no todos, display this message (&& = shortcircuiting) */}
+    {todos.length === 0 && "No Todos"}
+    {/* Loop through todos and render them out into list items */}
+    {todos.map(todo => {
+      return <li key={todo.id}>
       <label>
-        <input type="checkbox"></input>
-        Item 1
+        <input 
+        type="checkbox" 
+        checked={todo.completed} 
+        // On change, take in an event and call func toggleTodo (defined above return statement). This func takes in the todo's id and checks to see if the input is checked or not
+        onChange={e => toggleTodo(todo.id, e.target.checked)}
+        />
+        {todo.title}
       </label>
-      <button className="btn btn-danger">Delete</button>
+      <button 
+      // Usually want to pass a function into onClick, otherwise, we're calling deleteTodo() and then immediately passing the result/return value of that function as our click event listener.
+      onClick={() => deleteTodo(todo.id)}
+      className="btn btn-danger">Delete</button>
     </li>
-    <li>
-      <label>
-        <input type="checkbox"></input>
-        Item 2
-      </label>
-      <button className="btn btn-danger">Delete</button>
-    </li>
+    })}
   </ul>
   </>
   )
